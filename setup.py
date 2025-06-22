fe0f3e7231a9aca2b4e2649522b7df00f9f85322
Keith Henderson <keith.donaldh@gmail.com>
2025-06-22 17:11:48 -0400

    updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="piperss",
-    version="0.1.1",
+    version="0.1.2",
     author="Keith Henderson",
     description="PipeRSS is minimalistic terminal-based RSS reader.",
     license="MIT",
